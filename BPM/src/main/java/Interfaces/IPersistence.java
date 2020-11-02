4a00c58be9b9e61e530bf525f4722b79acf52963
Madsen <chmad18@student.sdu.dk>
2020-11-02 01:40:10 +0100

    Updated IPersistence

--- BPM/src/main/java/Interfaces/IPersistence.java
+++ BPM/src/main/java/Interfaces/IPersistence.java
@@ -1,4 +1,27 @@
 package Interfaces;
 
+import Domain.Batch;
+import Domain.Ingredient;
+import Domain.Production;
+
+import java.util.ArrayList;
+
 public interface IPersistence {
+    public Batch getBatch(int batchId);
+
+    public Production getProduction(int productionId);
+
+    public ArrayList<Batch> getBatches();
+
+    public ArrayList<Production> getProductions();
+
+    public ArrayList<Ingredient> getIngredients();
+
+    public void createBatch(Batch batch);
+
+    public void createProduction(Production production);
+
+    public void deleteBatch(int batchId);
+
+    public void deleteProduction(int productionId);
 }
