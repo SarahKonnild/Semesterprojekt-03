d0ad73ecebe42973d7c360fffe8e21f30b6988f3
Kasper Svane <svane.kasper20@gmail.com>
2020-11-06 11:52:31 +0100

    Made react app so it can be tested with Docker + Docker-compose

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+//Connection to MongoDB
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established succesfully");
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
