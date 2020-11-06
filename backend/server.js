f1571195fee6a4b004ac88c7eeec7d207affb0e0
Kasper Svane <svane.kasper20@gmail.com>
2020-11-06 15:26:28 +0100

    Full React and Server completed with Docker and Docker-compose

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 require('dotenv').config();
+const config = require('./config/db');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,11 +13,10 @@ app.use(cors());
 app.use(express.json());
 
 //Connection to MongoDB
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+mongoose.connect(config.db, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
 
-const connection = mongoose.connection;
-connection.once('open', () => {
+let db = mongoose.connection;
+db.once('open', () => {
     console.log("MongoDB database connection established succesfully");
 })
 
