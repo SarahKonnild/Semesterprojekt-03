a6f7effda2be7cf675695a79e1e73b417060421e
SimonQuvang <simon.quvang@gmail.com>
2020-11-14 21:12:33 +0100

    Wrote the variables in the switchcase in socketserver

--- BPM/src/main/java/Domain/SocketServer.java
+++ BPM/src/main/java/Domain/SocketServer.java
@@ -12,7 +12,7 @@ public class SocketServer {
 
     public static ServerSocket serverSocket;
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         listen();
     }
 
@@ -20,14 +20,14 @@ public class SocketServer {
      * Socket related things
      */
     //Sauce: https://gist.github.com/mpj/a979ded460dd52eb536a
-    public static void listen(){
-        try{
+    public static void listen() {
+        try {
             serverSocket = new ServerSocket(8000);
-        }catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        while(true){
-            try{
+        while (true) {
+            try {
                 final Socket socket = serverSocket.accept();
                 final InputStream inputStream = socket.getInputStream();
                 final InputStreamReader streamReader = new InputStreamReader(inputStream);
@@ -38,40 +38,72 @@ public class SocketServer {
                 while ((line = br.readLine()) != null) {
                     System.out.println(line);
                     lineArray = line.split(",");
-                    for(int i = 1; i < lineArray.length; i++){
+                    for (int i = 1; i < lineArray.length; i++) {
                         //TODO fix the regex so that it doesn't split the time into smaller unusable pieces of garbage
-                        String[] newArray = lineArray[i].split(":",1);
+                        String[] newArray = lineArray[i].split(":", 1);
                         System.out.println(lineArray[i]);
                         lineArray[i] = newArray[1];
                     }
-                    switch (lineArray[0]){
-                        case "/startProduction":
+                    switch (lineArray[0]) {
+                        case "/startProduction": {
                             System.out.println(lineArray[2]);
-                            Facade.getFacade().startProduction(Integer.parseInt(lineArray[1]), Date.valueOf(lineArray[2]), BeerType.valueOf(lineArray[3]), Integer.parseInt(lineArray[4]), Double.parseDouble(lineArray[5]), Integer.parseInt(lineArray[6]));
+                            int batchId = Integer.parseInt(lineArray[1]);
+                            Date timestamp = Date.valueOf(lineArray[2]);
+                            BeerType beerType = BeerType.valueOf(lineArray[3]);
+                            int batchSize = Integer.parseInt(lineArray[4]);
+                            double productionSpeed = Double.parseDouble(lineArray[5]);
+                            int productionId = Integer.parseInt(lineArray[6]);
+
+                            Facade.getFacade().startProduction(batchId, timestamp, beerType, batchSize, productionSpeed, productionId);
                             break;
-                        case "/stopProduction":
-                            Facade.getFacade().stopProduction(new Production(Integer.parseInt(lineArray[1]),new Batch(Integer.parseInt(lineArray[2]), Date.valueOf(lineArray[3]), BeerType.valueOf(lineArray[4]),
-                                    Integer.parseInt(lineArray[5]), Double.parseDouble(lineArray[6]), Integer.parseInt(lineArray[7]))));
+                        }
+                        case "/stopProduction": {
+                            int productionId = Integer.parseInt(lineArray[1]);
+                            int batchId = Integer.parseInt(lineArray[2]);
+                            Date startTime = Date.valueOf(lineArray[3]);
+                            BeerType beerType = BeerType.valueOf(lineArray[4]);
+                            int batchSize =  Integer.parseInt(lineArray[1]);
+                            double productionSpeed = Double.parseDouble(lineArray[5]);
+
+                            Batch batch = new Batch(batchId, startTime,beerType, batchSize, productionSpeed,productionId);
+                            Production production = new Production(productionId, batch);
+
+                            Facade.getFacade().stopProduction(production);
                             break;
-                        case "/detectMaintenanceStatus":
+                        }
+                        case "/detectMaintenanceStatus": {
                             Facade.getFacade().detectMaintenanceStatus();
                             break;
-                        case "/calculateErrorSpeed":
-                            Facade.getFacade().calculateErrorSpeed(BeerType.valueOf(lineArray[1]), Double.valueOf(lineArray[2]));
+                        }
+                        case "/calculateErrorSpeed": {
+                            BeerType beerType = BeerType.valueOf(lineArray[1]);
+                            double speed = Double.parseDouble(lineArray[2]);
+
+                            Facade.getFacade().calculateErrorSpeed(beerType, speed);
                             break;
-                        case "/calculateErrorMargin":
-                            Facade.getFacade().calculateErrorMargin(BeerType.valueOf(lineArray[1]), Integer.parseInt(lineArray[2]));
+                        }
+                        case "/calculateErrorMargin": {
+                            BeerType beerType = BeerType.valueOf(lineArray[1]);
+                            int batchSize = Integer.parseInt(lineArray[2]);
+
+                            Facade.getFacade().calculateErrorMargin(beerType, batchSize);
                             break;
-                        case "/caclulateOptimalSpeed":
-                            Facade.getFacade().calculateOptimalSpeed(BeerType.valueOf(lineArray[1]), Double.valueOf(lineArray[2]));
+                        }
+                        case "/caclulateOptimalSpeed": {
+                            BeerType beerType = BeerType.valueOf(lineArray[1]);
+                            double errorMargin = Double.parseDouble(lineArray[2]);
+
+                            Facade.getFacade().calculateOptimalSpeed(beerType, errorMargin);
                             break;
-                        default:
+                        }
+                        default: {
                             //TODO DEBUG THIS :3 uwu
                             System.out.println("OH YES, DADDY SVANE BAMBOOZLED YOU <3");
+                        }
                     }
                     System.out.println(line);
                 }
-            }catch(IOException e){
+            } catch (IOException e) {
                 e.printStackTrace();
 //                System.exit(-1);
             }
