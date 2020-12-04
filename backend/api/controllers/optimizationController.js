e18744eeed193d74dc92715e80e36b106952b4e0
Sarah Manon Pradel <sapra18@student.sdu.dk>
2020-12-04 12:20:39 +0100

    Implemented check on whether or not the optimal speed is realistic
    Now there is a check on the CalculateOptimalSpeed. Hence, the method now requires:
    - batch size
    - user-defined acceptable maximum error margin
    - maximum time available
    This calculates the speed that is needed to complete the batch in that allotted time, and uses this speed in the formula that was derived from the scatterplot. This will find the estimated errormargin that this speed will produce. 
    
    The function then checks if the margin is bigger than the user-defined acceptable margin: if it is, then a 404 Bad Request message is returned, whereas if it is equal to or less than the specified margin, then the speed is returned.

--- backend/api/controllers/optimizationController.js
+++ backend/api/controllers/optimizationController.js
@@ -31,10 +31,20 @@ exports.calculateEstimatedProductionTime = function(req,res){
     res.send({"time":time});
 }
 
-//REQUIRES A BATCH SIZE AND A MAXIMUM PRODUCTION TIME IN MINUTES. WILL RETURN THE LOWEST SPEED POSSIBLE FOR THIS CONFIGURATION.
+//REQUIRES A BATCH SIZE, A MAXIMUM ACCEPTABLE ERROR MARGIN AND A MAXIMUM PRODUCTION TIME IN MINUTES. WILL RETURN THE LOWEST SPEED POSSIBLE FOR THIS CONFIGURATION.
 exports.calculateOptimalSpeed = function(req,res){
     let batch = req.body.batch;
+    let margin = req.body.margin;
     let time = req.body.time;
     let speed = time/batch;
-    res.send({"speed":speed});
+
+    let calcMarg = ((3.12*Math.pow(10,-3)*Math.pow(speed,2)+0.0658*speed-3.54)/speed)*100;
+
+    if(calcMarg > margin){
+        //IF THE CALCULATED ERROR MARGIN IS BIGGER THAN THE ACCEPTABLE ONE (user-defined), THEN THIS IS A BAD REQUEST; THE SPEED IS NOT ALLOWED FOR THE GIVEN MARGIN
+        res.status(400).send('Bad Request');
+    }else if( calcMarg == margin || calcMarg < margin){
+        //IF THE CALCULATED ERROR MARGIN IS THE SAME AS THE SPECIFIED MARGIN, NO PROBLEMOS
+        res.send({"speed":speed});
+    }
 }
