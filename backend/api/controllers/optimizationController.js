4f624d8bd46d06d1b48cfbfba31eb5c7ab53b76e
Sarah Manon Pradel <sapra18@student.sdu.dk>
2020-12-04 12:06:21 +0100

    Optimization calculations set up. Needs testing
    A total of 5 optimization calculation functions can now be called. These are: 
    1. CalculateErrorSpeed, which takes in a batch size and an accepted error margin (in %) to produce a maximum speed that the user can run the machine at, without exceeding the acceptable error margin. 
    2. CalculateErrorMargin, which takes in a speed to produce a minimum amount of errors that can be produced at that speed.
    3. CalculateAmountOfErrors, which takes in a batch size and a margin, to calculate the amount of errors that the user produces for that batch, with the specified error margin
    4. CalculateEstimatedProductionTime, which takes in a batch size and a speed, calculating the minimum amount of time that it will take to produce the batch
    5. CalculateOptimalSpeed, which takes in a batch size and a time, and finds the minimum speed that the user will have to work with, to meet that deadline. 
    
    The way that these informations are combined must make up the optimization estimations in the frontend. As such, it becomes possible to estimate several parameters, with the most important being:
    
    The amount of errors for the optimal speed, which is found by:
    Receiving the input of a batch size and the time allotted to the production and an acceptable error margin. These parameters can of course be found by making a combination of the previous formulas. 
    IF THE USER allots a production time that provides a speed that produces a higher error margin than desired, the system should output an error (this must be implemented in the controller first however, see next commit) indicating whether or not this is a realistic goal.

--- backend/api/controllers/optimizationController.js
+++ backend/api/controllers/optimizationController.js
@@ -1,20 +1,40 @@
+//REQUIRES A MARGIN AND BATCH SIZE TO BE ENTERED (PERCENT) AND 
 exports.calculateErrorSpeed = function(req,res){
-    res.send("Ah yes Daddy Svane, I work");
-    let data = JSON.stringify(req.body);
-    console.log(data);
-    client.write(req.url + ", " + data + "\n");
+    let batch = req.body.batch;
+    let margin = req.body.margin;
+    let amount = (margin/100)*batch;
+    let speed = (6250*((39*amount)/3125 + 1212721/25000000)^(1/2))/39 - (1645/156);
+    res.send({"speed":speed});
 }
 
-exports.calculateErrorMargin = function(req,res){
-    res.send("Ah yes Daddy Svane, I work");
-    let data = JSON.stringify(req.body);
-    console.log(data);
-    client.write(req.url + ", " + data + "\n");
+//REQUIRES A SPEED TO BE SENT IN. WILL RETURN A MARGIN IN PERCENTAGE
+exports.cacluateErrorMargin = function(req,res){
+    let speed = req.body.speed;
+    //Sends the error margin in percentage of beers produced
+    let margin = ((3.12*Math.pow(10,-3)*Math.pow(speed,2)+0.0658*speed-3.54)/speed)*100;
+    res.send({"margin":margin});
 }
 
+//REQUIRES A BATCH SIZE TO BE SENT IN AND A MARGIN IN PERCENTAGE. WILL RETURN AN AMOUNT OF ERROR BEERS.
+exports.calculateAmountOfErrors = function(req,res){
+    let margin = req.body.margin;
+    let batch = req.body.batch;
+    let amount = (margin/100)*batch;
+    res.send({"errors":amount});
+}
+
+//REQUIRES A BATCH SIZE AND A SPEED INDICATION. WILL RETURN AN AMOUNT OF TIME IN MINUTES.
+exports.calculateEstimatedProductionTime = function(req,res){
+    let batch = req.body.batch;
+    let speed = req.body.speed;
+    let time = batch/speed;   
+    res.send({"time":time});
+}
+
+//REQUIRES A BATCH SIZE AND A MAXIMUM PRODUCTION TIME IN MINUTES. WILL RETURN THE LOWEST SPEED POSSIBLE FOR THIS CONFIGURATION.
 exports.calculateOptimalSpeed = function(req,res){
-    res.send("Ah yes Daddy Svane, I work");
-    let data = JSON.stringify(req.body);
-    console.log(data);
-    client.write(req.url + ", " + data + "\n");
+    let batch = req.body.batch;
+    let time = req.body.time;
+    let speed = time/batch;
+    res.send({"speed":speed});
 }
