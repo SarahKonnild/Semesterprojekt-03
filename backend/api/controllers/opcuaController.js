ba500d1b3350215d0095cffa1fd55de76952f183
SimonQuvang <simon.quvang@gmail.com>
2020-12-08 16:50:44 +0100

    Read the description. New try catch stuff added plus comments
    What changes did I make? 
    Moved some nodeID's variables from global to locally now that there only is 1 function that needed them. This was for change state, change command and currentstate. 
    Added comments to all functions that describe what the function does. 
    Added comments to most of the code to explain what the code does. 
    Added a nested try catch statement in startsession
    Added another thing to the json object that the exported function returns in case of an error is catched. 
    
    Why did I make those changes? 
    Comments are needed to document what the code does. Also so people have an easier time understanding the code. 
    
    The nested try catch statement is added so the code will now try to connect to the simulation first, if it cant, an error should happen, that we then catch. We will then try to connect to the physical machine instead. 
    
    The extra json object is just the error message that is given if it catch an error

--- backend/api/controllers/opcuaController.js
+++ backend/api/controllers/opcuaController.js
@@ -1,31 +1,61 @@
 
 const nodeOPCUA = require ('../../nodeopcua.ts');
-
+/**
+ * @author Simon Quvang
+ * 
+ * 
+ * @param req this parameter must include a JSON formatted object: {"beers":int, "speed":int, "batchnumber":int, "beertype":int}
+ * @param res this parameter will send back a JSON formatted object: {"statusCode":int, "measage":string}
+*/
 exports.startProduction = async function(req,res){
-    // TODO Add some way of getting the data from the req to the function call
     //beers, productionSpeed, batchnumber, beerType
     let someValue = await nodeOPCUA.startProduction(req.body.beers, req.body.speed, req.body.batchnumber, req.body.beerType);
     res.send(someValue)
     res.end;
 };
-
+/**
+ * @author Simon Quvang
+ * 
+ * 
+ * @param req Dosent take any additional parameters
+ * @param res this parameter will send back a JSON formatted object: {"statusCode":int, "measage":string}
+*/
 exports.stopProduction = async function(req,res){
     returnValue = await nodeOPCUA.stopProduction();
     res.send(returnValue);
     res.end;
 }
-
+/**
+ * @author Simon Quvang
+ * 
+ * 
+ * @param req Dosent take any additional parameters
+ * @param res this parameter will send back a JSON formatted object: {"statusCode":int, "measage":string}
+*/
 exports.resetProduction = async function(req, res){
     returnValue = await nodeOPCUA.resetProduction();
     res.send(returnValue);
     res.end;
 }
-
+/**
+ * @author Simon Quvang
+ * 
+ * 
+ * @param req this function do not need anything passed along with the get request
+ * @param res this parameter will send back a JSON formatted object: {"statusCode":int, "measage":string, "maintenacneStatus": value}
+*/
 exports.detectMaintenanceStatus = async function(req,res){
     returnValue = await nodeOPCUA.getMaintenanceStatus();
     res.send(returnValue);
     res.end;
 }
+/**
+ * @author Simon Quvang
+ * 
+ * 
+ * @param req additional
+ * @param res this parameter will send back a JSON formatted object: {"statusCode":int, "measage":string}
+*/
 exports.getProductionCount = async function(req,res){
     returnValue = await nodeOPCUA.getProducedAmount();
     res.send(returnValue);
